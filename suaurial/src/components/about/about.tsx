4b1ae640db640cf63b7994fb0f9fa9f3d1f880ae
Su Yang <syang@figma.com>
2022-09-04 21:25:54 -0700

    about

--- suaurial/src/components/about/about.tsx
+++ suaurial/src/components/about/about.tsx
@@ -5,18 +5,18 @@ import image from "./images/image.jpg";
 export const About = () => {
   return (
     <div className={styles.about}>
+      <img className={styles.image} src={image} alt="Headshot" />
       <div className={styles.paragraphs}>
         <p className={styles.paragraph}>Hello! I’m Su.</p>
         <p className={styles.paragraph}>
           I’m a still life and travel photographer based out of Northern
-          California. My photographs are inspired by natural light, movement,
-          stillness, and culture.
+          California. My photographs are inspired by natural light,
+          juxtaposition, stillness, and culture.
         </p>
         <p className={styles.paragraph}>
           For all inquiries, please contact suyang581@gmail.com.
         </p>
       </div>
-      <img className={styles.image} src={image} alt="Headshot" />
     </div>
   );
 };
